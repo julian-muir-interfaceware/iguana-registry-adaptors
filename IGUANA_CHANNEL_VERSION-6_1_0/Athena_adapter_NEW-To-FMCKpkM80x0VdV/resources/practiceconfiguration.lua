a060ce1a31035865bdce5ed0108f5bd2089c4003
julian-muir-interfaceware <julian.muir@interfaceware.com>
2021-09-07 17:18:54 +0200

    Delete IGUANA_CHANNEL_VERSION-6_1_0 directory

--- IGUANA_CHANNEL_VERSION-6_1_0/Athena_adapter_NEW-To-FMCKpkM80x0VdV/resources/practiceconfiguration.lua
+++
@@ -1,8 +0,0 @@
-local practiceconfig = {}
-
---EXAMPLE: GET available practice IDs /v1/{practiceid}/practiceinfo
-function practiceconfig.getPracticeInfo(api, practiceId)
-   local practice = api["practice configuration"].practiceinfo.read{practiceid = practiceId}
-   return practice
-end
-return practiceconfig
